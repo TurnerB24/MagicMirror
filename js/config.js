21ba4b79282298bdf7c5093c03ea6f95ee3a2798
Turner Bowling <turnerb24@gmail.com>
2015-07-10 17:26:06 -0500

    Update config.js
    Changed location from Netherlands to school :)

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ var lang = window.navigator.language;
 //change weather params here:
 //units: metric or imperial
 var weatherParams = {
-    'q':'Baarn,Netherlands',
+    'q':'Blacksburg, Virginia, USA',
     'units':'metric',
     'lang':lang
 };
